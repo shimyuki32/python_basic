51ee2759283a3dbee694bfb625127d535c8e4e37
shimyuki32 <shimyuki32@gmail.com>
2022-05-03 12:30:57 +0900

    05/13

---
+++ C1/archive.py
@@ -0,0 +1,19 @@
+import json
+import webbrowser 
+from urllib.request import urlopen
+
+print("Let's find an old website")
+site = input("Type a website URL: ")
+era = input("Type a year, month, and day, like 20150613: ")
+url = f"http://archive.org/wayback/available?url={site}&timestamp={era}"
+response = urlopen(url)
+contents = response.read()
+text = contents.decode("utf-8")
+data = json.loads(text)
+try:
+    old_site = data["archived_snapshots"]["closest"]["url"]
+    print("FOund this copy: ", old_site)
+    print("It should appear in your browser now.")
+    webbrowser.open(old_site)
+except:
+    print("Sorry, no luck finding", site)
